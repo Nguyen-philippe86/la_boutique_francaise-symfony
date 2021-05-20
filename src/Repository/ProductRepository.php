369d24aae578af6e096b4e09e5580d8d228eb42e
Nguyen <philippe.nguyen86@gmail.com>
2021-05-20 17:24:28 +0200

    ajout composant filtre de recherche

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -2,13 +2,14 @@
 
 namespace App\Repository;
 
+use App\Classe\Search;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Product|null find($id, $lockMode = null, $lockVersion = null)
- * @method Product|null findOneBy(array $criteria, array $orderBy = null)
+ * @method null|Product find($id, $lockMode = null, $lockVersion = null)
+ * @method null|Product findOneBy(array $criteria, array $orderBy = null)
  * @method Product[]    findAll()
  * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
@@ -17,6 +18,35 @@ class ProductRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+
+    /**
+     * Requête de récupération les produits de la recherche de l'utilisateur.
+     *
+     * @return Product[]
+     */
+    public function findWithSearch(Search $search)
+    {
+        $query = $this
+            ->createQueryBuilder('p')
+            ->select('c', 'p')
+            ->join('p.category', 'c')
+        ;
+
+        if (!empty($search->categories)) {
+            $query = $query
+                ->andWhere('c.id IN (:categories)')
+                ->setParameter('categories', $search->categories)
+            ;
+        }
+        if (!empty($search->string)) {
+            $query = $query
+                ->andWhere('p.name LIKE :string')
+                ->setParameter('string', "%{$search->string}%")
+            ;
+        }
+
+        return $query->getQuery()->getResult();
     }
 
     // /**
