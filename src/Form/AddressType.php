4d687f07aabb2d7bc76cda473a62da9b7d258bcd
Nguyen <philippe.nguyen86@gmail.com>
2021-05-21 15:59:28 +0200

    ajout composant gestion des adresses

---
+++ src/Form/AddressType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddressType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Quel nom souhaitez-vous donner à votre adresse ?',
+                'attr' => [
+                    'placeholder' => 'Nommez votre adresse',
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom',
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Votre nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom',
+                ],
+            ])
+            ->add('company', TextType::class, [
+                'label' => 'Votre société',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom de votre société (falcultatif)',
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Votre adresse',
+                'attr' => [
+                    'placeholder' => '56 rue Victor H...',
+                ],
+            ])
+            ->add('postal', TextType::class, [
+                'label' => 'Votre code postal',
+                'attr' => [
+                    'placeholder' => 'Entrer votre code postal',
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Entrer votre ville',
+                ],
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'Entrer votre pays',
+                ],
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Votre téléphone',
+                'attr' => [
+                    'placeholder' => 'Entrer votre numéro de téléphone',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn-block btn-info mt-3',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+        ]);
+    }
+}
